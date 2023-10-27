a442e558a95a2fe5a08f8319bd42843678021cf1
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-10-27 15:30:47 +0600

    successfully implemented admin panel and user sign up operation

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item
-
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[:6]
@@ -10,6 +10,22 @@ def index(request):
         'items': items,
     })
 
+
 def contact(request):
     return render(request, "core/contact.html")
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
