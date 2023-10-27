d0505d7f6acdfedfae44a6de0ef18f2c1bee3041
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-10-27 08:54:00 +0600

    successfully showed all the products on index.html pages and also created the details page of each products

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 
 def contact(request):
     return render(request, "core/contact.html")
+
---
+++ item/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Item
+
+def details(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[:3]
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
