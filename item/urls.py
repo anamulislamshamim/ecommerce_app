72012f9fd28f65f9777a65ff7e9cd853c3c40888
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-10-28 15:48:02 +0600

    Implemented the delete operation

--- item/urls.py
+++ item/urls.py
@@ -7,4 +7,5 @@ app_name = 'item'
 urlpatterns = [
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.details, name='details'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
 ]
