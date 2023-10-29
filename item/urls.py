ae85056201ebbc6f927427a714b14cbecf3a4987
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-10-29 20:14:07 +0600

    completed the search or browse functionality

--- item/urls.py
+++ item/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
+    path('', views.items, name='items'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.details, name='details'),
     path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,9 +1,30 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q 
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
-from .models import Item
+from .models import Item, Category
+
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
 
 
 def details(request, pk):
@@ -34,6 +55,25 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': "Add New Item"
+    })
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:details', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+    return render(request, 'item/form.html', {
+        'form': form, 
+        'title': 'Edit item'
     })
 
 
